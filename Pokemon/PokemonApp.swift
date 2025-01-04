e8842ab5b74d5a3804dc3f7dc18e4cd6007a9741
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2025-01-04 22:34:23 +0200

    first commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by mohamed ramadan on 04/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonTests/PokemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTests.swift
+//  PokemonTests
+//
+//  Created by mohamed ramadan on 04/01/2025.
+//
+
+import Testing
+@testable import Pokemon
+
+struct PokemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
